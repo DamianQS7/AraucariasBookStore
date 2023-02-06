f0bb29cbbcdb3cdcc1045f94dde70d54ad125664
DamianQS7 <102097286+DamianQS7@users.noreply.github.com>
2023-02-06 14:57:06 -0500

    Initial Commit: Pushing up the project

---
+++ AraucariasBookStore/AraucariasBookStoreWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AraucariasBookStoreWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ AraucariasBookStore/AraucariasBookStoreWeb/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using AraucariasBookStoreWeb.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//Setting up the database before the app builds.
+string? connStr = builder.Configuration.GetConnectionString("ConnString");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connStr));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
